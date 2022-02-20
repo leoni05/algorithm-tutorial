890d9fa8633fd0d9699529d0eacc5ea09a1782b0
yongjun18 <yongjun18976@gmail.com>
2022-02-20 16:42:52 +0900

    mouse hover효과 적용을 위한 테스트
    svg pattern을 이용하여 격자를 그린 다음, svg 자체에 그라데이션 효과 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <div className="social-line">
@@ -8,7 +9,24 @@ function App() {
         <span>Contact</span>
       </div>
       <div className="title">
-        <span>ALGORITHM</span>
+        <span>ALGORITHM
+
+          <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <radialGradient id="RadialGradient1">
+                <stop offset="0%" stop-color="red"/>
+                <stop offset="100%" stop-color="blue"/>
+              </radialGradient>
+
+              <pattern id="grid" width="8" height="8" patternUnits="userSpaceOnUse">
+                <path d="M 8 0 L 0 0 0 8" fill="none" stroke="#D2DCE5" stroke-width="1"/>
+              </pattern>
+            </defs>
+            
+            <rect width="100%" height="100%" fill="url(#grid)" />
+          </svg>
+
+        </span>
       </div>
     </div>
   );
