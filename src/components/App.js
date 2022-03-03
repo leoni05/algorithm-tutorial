050abc52b7a89ca2e4f166ce98cb25ce3b56db4e
yongjun18 <yongjun18976@gmail.com>
2022-03-03 22:39:49 +0900

    임시 Slider 컴포넌트 완성

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Title from './Title';
 import SocialLine from './SocialLine';
 import TitleBar from './TitleBar';
+import Slider from './Slider';
 
 function App() {
   return (
     <div className="App">
       <SocialLine/>
       <TitleBar/>
+      <Slider/>
     </div>
   );
 }
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Slider.css';
+
+function Slider() {
+  return (
+    <div className="slider">
+      <span className="slide-item">Stack</span>
+      <span className="slide-item">Segment Tree</span>
+      <span className="slide-item">Stack</span>
+    </div>
+  );
+}
+
+export default Slider;
