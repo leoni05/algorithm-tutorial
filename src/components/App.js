915daa73f80906d519815789e6428a98402d352d
yongjun18 <yongjun18976@gmail.com>
2022-03-04 21:51:50 +0900

    임시 Content, Description 컴포넌트 작성 중

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import Title from './Title';
 import SocialLine from './SocialLine';
 import TitleBar from './TitleBar';
 import Slider from './Slider';
+import Content from './Content';
 
 function App() {
   return (
     <div className="App">
       <SocialLine/>
       <TitleBar/>
-      <Slider/>
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Content/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Content.css';
+import Description from '../Description';
+
+function Content() {
+  return (
+    <div className="content">
+      <Description/>
+    </div>
+  );
+}
+
+export default Content;
