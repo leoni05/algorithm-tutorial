98b300e2209f61cdf2a1a44af51f78391aa9e726
yongjun18 <yongjun18976@gmail.com>
2022-03-09 17:05:25 +0900

    Route 적용 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+
 import './App.css';
 import Title from './Title';
 import SocialLine from './SocialLine';
@@ -7,13 +10,21 @@ import Slider from './Slider';
 import Content from './Content';
 import NavSlider from './NavSlider';
 
+const routes = [
+  { path: '/*', key: "SocialLine", Component: SocialLine },
+  { path: '/*', key: "Title", Component: Title },
+];
+
 function App() {
   return (
     <div className="App">
-      <SocialLine/>
-      <TitleBar/>
-      <Content/>
-      <NavSlider/>
+      <Router>
+        {routes.map(({ path, key, Component }) => (
+          <Routes key={key}>
+            <Route path={path} element={<Component/>}></Route>
+          </Routes>
+        ))}
+      </Router>
     </div>
   );
 }
