ff4539fafdee5446e9a72ad37e33ecb35e77c386
yongjun18 <yongjun18976@gmail.com>
2022-03-11 16:35:19 +0900

    Title component 애니메이션 초안

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -41,26 +41,27 @@ function Title() {
   return (
     <CSSTransition in={location.pathname == "/"} timeout={300} classNames="test" nodeRef={nodeRef}>
       <div className="title-wrapper" ref={nodeRef}>
-        <div className="title" onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
-          <div className="in-title"></div>
+        <div className="title-container" onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
 
+        <span className="title">
           <Link to={location.pathname == "/" ? "/algorithms" : "/"} className="text-link">
             <span>ALGORITHM</span>
           </Link>
+        </span>
 
-          <svg style={svgStyle} xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <radialGradient id="RadialGradient1">
-                <stop offset="0%" stopColor="red"/>
-                <stop offset="100%" stopColor="blue"/>
-              </radialGradient>
-              <pattern id="grid" width="8" height="8" patternUnits="userSpaceOnUse">
-                <path d="M 8 0 L 0 0 0 8" fill="none" stroke="#D2DCE5" strokeWidth="1.5"/>
-              </pattern>
-            </defs>
-            <rect width="100%" height="100%" fill="url(#grid)" />
-          </svg>
-
+        <svg style={svgStyle} xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <radialGradient id="RadialGradient1">
+              <stop offset="0%" stopColor="red"/>
+              <stop offset="100%" stopColor="blue"/>
+            </radialGradient>
+            <pattern id="grid" width="8" height="8" patternUnits="userSpaceOnUse">
+              <path d="M 8 0 L 0 0 0 8" fill="none" stroke="#D2DCE5" strokeWidth="1.5"/>
+            </pattern>
+          </defs>
+          <rect width="100%" height="100%" fill="url(#grid)" />
+        </svg>
+        
         </div>
       </div>
     </CSSTransition>
