c0d08e2a14e108763d165e9811bd6a16bf661296
yongjun18 <yongjun18976@gmail.com>
2022-03-13 12:39:19 +0900

    TitleBar import문 제거

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import { CSSTransition } from 'react-transition-group'
 import './App.css';
 import Title from './Title';
 import SocialLine from './SocialLine';
-import TitleBar from './TitleBar';
 import Slider from './Slider';
 import Content from './Content';
 import NavSlider from './NavSlider';
