ee46ba1b0a7762dc60853b77636ff8850b22cd1e
yongjun18 <yongjun18976@gmail.com>
2022-08-20 16:35:15 +0900

    title 애니메이션 200ms 설정

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -45,7 +45,7 @@ function Title() {
   }, []);
 
   return (
-    <CSSTransition in={location.pathname === "/"} timeout={500} classNames="title" nodeRef={nodeRef}>
+    <CSSTransition in={location.pathname === "/"} timeout={200} classNames="title" nodeRef={nodeRef}>
       <div className={titleWrapperClasses} ref={nodeRef}>
         <div className="title-container" onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
 
