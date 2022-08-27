95b4bbea6060792bc947c6972edac282531ace02
yongjun18 <yongjun18976@gmail.com>
2022-08-27 20:30:06 +0900

    Content 컴포넌트 init-enter, init-exit 추가

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,12 +8,16 @@ import { Link, useLocation } from 'react-router-dom'
 function Content() {
   let location = useLocation();
 
+  const contentWrapperClasses = "content-wrapper " +
+    ((location.pathname === "/algorithms/contents/seg") ?
+      "content-init-enter" : "content-init-exit");
+
   return (
 
     <CSSTransition in={location.pathname === "/algorithms/contents/seg"}
       timeout={400} classNames="content">
 
-    <div className="content-wrapper">
+    <div className={contentWrapperClasses}>
       <div className="content">
         <Description className="description"/>
         <ShowContainer className="show-container"/>
