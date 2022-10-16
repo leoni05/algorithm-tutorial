152d1b23c2bd28cdfae3cb85706117b1fcedbd3e
yongjun18 <yongjun18976@gmail.com>
2022-10-16 20:53:00 +0900

    알고리즘 전환 애니메이션 구현

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Content.css';
 import Description from '../Description';
 import ShowContainer from '../ShowContainer';
@@ -10,14 +10,37 @@ function Content() {
   let location = useLocation();
   let algorithms = useAlgorithms();
   const nodeRef = React.useRef(null);
+  const [inProps, setInProps] = useState(
+    algorithms.isShowingAlgorithm(location.pathname)
+  );
+  const [pvPathname, setPvPathname] = useState('');
+  const [firstExecuted, setFirstExecuted] = useState(true);
 
   const contentWrapperClasses = "content-wrapper " +
-    ((location.pathname === "/algorithms/contents/seg") ?
+    ((algorithms.isShowingAlgorithm(location.pathname)) ?
       "content-init-enter" : "content-init-exit");
+
+  useEffect(() => {
+    if (firstExecuted){
+      setFirstExecuted(false);
+      return;
+    }
+    let delay = 0;
+
+    setInProps(false);
+    if (algorithms.isShowingAlgorithm(pvPathname)) {
+      delay = 400;
+    }
+    if(algorithms.isShowingAlgorithm(location.pathname)){
+      setTimeout(()=>{ setInProps(true);}, delay);
+    }
+
+    setPvPathname(location.pathname)
+  }, [location.pathname]);
 
   return (
 
-    <CSSTransition in={location.pathname === "/algorithms/contents/seg"}
+    <CSSTransition in={inProps}
       timeout={400} classNames="content" nodeRef={nodeRef}>
 
     <div className={contentWrapperClasses} ref={nodeRef}>
