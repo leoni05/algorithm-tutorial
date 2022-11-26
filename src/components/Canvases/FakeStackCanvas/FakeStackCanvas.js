9973262a730af746e413916d2708636c31319281
yongjun18 <yongjun18976@gmail.com>
2022-11-26 23:18:24 +0900

    fake stack canvas 추가

---
+++ src/components/Canvases/FakeStackCanvas/FakeStackCanvas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FakeStackCanvas.css';
+import stack from '../../../img/stack.svg';
+
+function FakeStackCanvas() {
+  return (
+    <img src={stack} className="show-canvas"></img>
+  );
+}
+export default FakeStackCanvas;
